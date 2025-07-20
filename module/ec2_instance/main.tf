c348281f7c3bc83d1ff23c6edd52c70c149a486e
Arunprakashreddy <reddyarunprakash@gmail.com>
2025-07-20 09:27:21 +0530

    This is the fifth commit

--- module/ec2_instance/main.tf
+++ module/ec2_instance/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "chandu_public_server" {
   ami = var.ami_public
   instance_type = var.instance_type_public
-  #key_name = file("aws_keys.pub")
+  key_name = "chandu_keys"  #generate aws key manually and then apply
   subnet_id = var.subnet_id_public
   security_groups = [var.security_groups_id]
 }
